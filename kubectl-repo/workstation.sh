b9817fe8a51db9230e1c3b92740254c7005444af
phanis-git <phanipspkmec@gmail.com>
2026-09-19 15:06:04 +0530

    docker image

--- kubectl-repo/workstation.sh
+++ kubectl-repo/workstation.sh
@@ -18,3 +18,16 @@ PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+
+# If any problem in kubectl and eksctl 
+# =====================================
+
+# mkdir -p $HOME/bin && cp /usr/local/bin/kubectl $HOME/bin/
+# sudo mv /usr/local/bin/kubectl /usr/bin/
+
+# kubectl version --client
+
+
+# cp /usr/local/bin/eksctl $HOME/bin/
+# eksctl version
