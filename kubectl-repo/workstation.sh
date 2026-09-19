85dcb8ad0865960353a15b4673f8abd873b73a3a
phanis-git <phanipspkmec@gmail.com>
2026-09-19 13:53:10 +0530

    docker image

--- kubectl-repo/workstation.sh
+++ kubectl-repo/workstation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-growpart /dev/nvme0n1 4
-lvextend -L +30G /dev/mapper/RootVG-varVol
-xfs_growfs /var
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -L +30G /dev/mapper/RootVG-varVol
+sudo xfs_growfs /var
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
